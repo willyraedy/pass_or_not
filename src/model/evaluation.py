4eaa178dd4eb829f99f125be05075b3c71b31eda
Willy Raedy <wraedy@gmail.com>
2020-02-08 14:05:52 -0600

    notebook died

--- src/model/evaluation.py
+++ src/model/evaluation.py
@@ -7,9 +7,10 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.preprocessing import StandardScaler
 from collections import defaultdict
+from imblearn.over_sampling import RandomOverSampler
 
 all_proba_metrics = ['roc_auc']
-all_non_proba_metrics = ['accuracy', 'precision']
+all_non_proba_metrics = ['accuracy', 'precision', 'recall']
 
 
 def cross_validate(
@@ -20,7 +21,10 @@ def cross_validate(
   fit_params={},
   cv=5,
   standardize=False,
-  stratify_on_target=True
+  stratify_on_target=True,
+  oversample=False,
+  show_conf_matrices=False,
+  threshold=0.5
 ):
     """
     Implements K Fold cross validation, returns metrics and estimators
@@ -44,20 +48,39 @@ def cross_validate(
             scaler = StandardScaler()
             X_tr = scaler.fit_transform(X_tr)
             X_val = scaler.transform(X_val)
+        if oversample:
+            ros = RandomOverSampler(random_state=0)
+            X_tr, y_tr = ros.fit_sample(X_tr,y_tr)
 
         lm = estimator(**fit_params)
         fit = lm.fit(X_tr, y_tr)
+        model_has_pred_proba = getattr(lm, 'predict_proba', None) and callable(lm.predict_proba)
 
-        in_sample_preds = fit.predict(X_tr)
-        out_sample_preds = fit.predict(X_val)
+        if threshold != 0.5:
+          assert model_has_pred_proba, 'Model does not allow threshold tuning'
+          apply_thresh = np.vectorize(lambda x: 1 if x >= threshold else 0)
+          in_sample_preds = apply_thresh(fit.predict_proba(X_tr)[:, 1])
+          out_sample_preds = apply_thresh(fit.predict_proba(X_val)[:, 1])
+        else:
+          in_sample_preds = fit.predict(X_tr)
+          out_sample_preds = fit.predict(X_val)
+
         for metric in non_proba_metrics:
           score_fn = getattr(metrics, metric + '_score')
           results['test_' + metric].append(score_fn(y_true=y_val, y_pred=out_sample_preds))
           results['train_' + metric].append(score_fn(y_true=y_tr, y_pred=in_sample_preds))
 
-        if getattr(lm, 'predict_proba', None) and callable(lm.predict_proba):
+        if show_conf_matrices:
+          print_confusion_matrix(
+            metrics.confusion_matrix(y_val, out_sample_preds),
+            ['no_third', 'yes_third'],
+            'Confusion Matrix'
+          )
+
+        if model_has_pred_proba:
           in_sample_preds = fit.predict_proba(X_tr)
           out_sample_preds = fit.predict_proba(X_val)
+
           for metric in proba_metrics:
             score_fn = getattr(metrics, metric + '_score')
             results['test_' + metric].append(score_fn(y_true=y_val, y_score=out_sample_preds[:, 1]))
@@ -69,21 +92,21 @@ def cross_validate(
         results['y_true'].append(y_val)
         results['X_test'].append(X_val)
 
-
     return results
 
-
-def make_confusion_matrix(X_test, y_test, model, labels, title, threshold=0.5):
-    y_predict = (model.predict_proba(X_test)[:, 1] >= threshold)
-    cmx = metrics.confusion_matrix(y_test, y_predict)
+def print_confusion_matrix(conf_mx, labels, title):
     plt.figure(dpi=80)
-    sns.heatmap(cmx, cmap='Blues', annot=True, square=True, fmt='d',
+    sns.heatmap(conf_mx, cmap='Blues', annot=True, square=True, fmt='d',
            xticklabels=labels,
            yticklabels=labels)
     plt.title(title)
     plt.xlabel('prediction')
     plt.ylabel('actual')
 
+def make_confusion_matrix(X_test, y_test, model, labels, title, threshold=0.5):
+    y_predict = (model.predict_proba(X_test)[:, 1] >= threshold)
+    cmx = metrics.confusion_matrix(y_test, y_predict)
+    print(cmx, labels, title)
 
 def report_single_model_metrics(scores, metrics=['roc_auc', 'accuracy', 'precision']):
     rows = []
@@ -151,3 +174,35 @@ def run_pipeline(
     # return formatted results
     return report_single_model_metrics(scores_tuned)
 
+def comparison_pipeline(X, y, models, metrics):
+  results = []
+  other_info = []
+  for name, param_grid, estimator in models:
+      # standardize if model requires it
+      std = StandardScaler()
+      X_tr = std.fit_transform(X) if name in ['knn', 'log', 'svm'] else X
+
+      _, best_params = report_grid_results(
+          X_tr,
+          y,
+          estimator(),
+          param_grid=param_grid,
+      )
+      scores = cross_validate(
+          estimator,
+          X_tr,
+          y,
+          fit_params=best_params,
+          scoring=metrics,
+          standardize=False
+      )
+      other_info.append({'model': name, 'scores': scores, 'best_params': best_params })
+
+      # format results into a single row
+      res = report_single_model_metrics(scores)
+      res = res[['mean']].transpose()
+      res.columns = ['roc_auc', 'accuracy', 'precision']
+      res.index = [name]
+
+      results.append(res)
+  return pd.concat(results), other_info
